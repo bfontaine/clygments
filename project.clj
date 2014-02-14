8e8207e4fecee4f5479a5c2f8181df7cc6332a2d
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-15 00:24:46 +0100

    coveralls reporting fixed

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies [[org.clojure/clojure          "1.5.1"]
                  [org.python/jython-standalone "2.5.3"]
                  [org.pygments/pygments        "1.6"]]
-  :plugins [[lein-cloverage "1.0.0"]])
+  :plugins [[lein-cloverage "1.0.2"]])
