bdfee19e8395c33f124e549db58fb3b19f080930
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-15 00:17:56 +0100

    lein-cloverage is needed for coveralls report

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure          "1.5.1"]
                  [org.python/jython-standalone "2.5.3"]
-                 [org.pygments/pygments        "1.6"]])
+                 [org.pygments/pygments        "1.6"]]
+  :plugins [[lein-cloverage "1.0.0"]])
