1813f628d9fbc13039ebdbe66da82c1651249c34
Baptiste Fontaine <b@ptistefontaine.fr>
2017-05-21 15:30:49 +0200

    Bump dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clygments "0.1.2-SNAPSHOT"
   :description "Use Pygments from Clojure"
-  :url "http://github.com/bfontaine/clygments"
+  :url "https://github.com/bfontaine/clygments"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure          "1.5.1"]
-                 [org.python/jython-standalone "2.5.3"]
-                 [org.pygments/pygments        "1.6"]]
-  :plugins [[lein-cloverage "1.0.2"]])
+  :dependencies [[org.clojure/clojure          "1.8.0"]
+                 [org.python/jython-standalone "2.7.0"]
+                 [org.pygments/pygments        "2.1.3"]]
+  :plugins [[lein-cloverage "1.0.9"]])
