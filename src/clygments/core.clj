d40f8305e13ff7d22acc7b86953a63cded57a040
Baptiste Fontaine <b@ptistefontaine.fr>
2019-03-18 22:38:08 +0100

    Refactor the code

--- src/clygments/core.clj
+++ src/clygments/core.clj
@@ -1,82 +1,87 @@
 (ns clygments.core
-  (:require [clojure.string :as cs])
-  (:import [org.python.util PythonInterpreter]))
+  (:require [clojure.string :as str])
+  (:import [org.python.util PythonInterpreter]
+           [org.python.core PyObject]))
 
-(def ^{:private true} python
+(def ^:private python
   (doto (PythonInterpreter.)
-    (.exec (str "from pygments import highlight\n"
-                "from pygments.lexers import get_lexer_by_name\n"
-                "from pygments.formatters import get_formatter_by_name\n"))))
+    (.exec "
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name as get_lexer_by_name_
+from pygments.formatters import get_formatter_by_name as get_formatter_by_name_
 
-(defn- exec-code-in
-  "put the result of the execution of a line of code in a variable, with
-   an optional default value if it encounters an exception"
-  ([var-name code]
-    (.exec python (str var-name "=" code)))
-  ([var-name code default]
-    (.exec python (str "try:\n\t" var-name "=" code "\n"
-                       "except:\n\t" var-name "=" default "\n"))))
+def get_lexer_by_name(name, opts):
+    try:
+        return get_lexer_by_name_(name, **opts)
+    except:
+        pass
 
-(def ^{:private true} py-true (.eval python "True"))
-(defn- py-true?
-  "test if one or more expressions eval to True in Python"
-  [& exprs]
-  (every? #(= (.eval python %) py-true) exprs))
+def get_formatter_by_name(name, opts):
+    try:
+        return get_formatter_by_name_(name, **opts)
+    except:
+        pass
 
-(defn- escape-string
-  "escape backslashes and double quotes in a string"
-  [s]
-  (-> s
-    (cs/replace #"\\" "\\\\\\\\") ; replace \ with \\
-    (cs/replace #"\"" "\\\\\""))) ; replace " with \"
+def run(code, lexer_name, lexer_opts, formatter_name, formatter_opts):
+    lexer = get_lexer_by_name(lexer_name, lexer_opts)
+    formatter = get_formatter_by_name(formatter_name, formatter_opts)
 
-(defn- keyword->argname
-  "convert a Clojure keyword into an argument name for Pygments’ highlight
-   function."
-  [kw]
-  (-> (name kw)
-    (cs/replace #"-" "")
-    (.toLowerCase)))
-
-(defn- keyword->langname
-  "convert a keyword into a syntaxically valid language name for Pygments. This
-   doesn’t ensure that the language is supported by Pygments."
-  [k]
-  (.toLowerCase (name k)))
+    if lexer and formatter:
+        return highlight(code, lexer, formatter)")))
 
 (defn- val->string
   "convert a simple Clojure value into a stringified Python one"
   [v]
   (cond
+    (string? v)  (pr-str v)
     (nil? v)     "None"
     (true? v)    "True"
     (false? v)   "False"
     (number? v)  (str v)
-    (string? v)  (str \" (escape-string v) \")
     (char? v)    (recur (str v))
     (keyword? v) (recur (name v))))
+
+(defn- keyword->argname
+  "convert a Clojure keyword into an argument name for Pygments’ highlight
+  function."
+  [kw]
+  (-> (name kw)
+      (str/replace #"-" "")
+      (str/lower-case)
+      val->string))
+
+(defn- keyword->langname
+  "convert a keyword into a syntaxically valid language name for Pygments. This
+   doesn’t ensure that the language is supported by Pygments."
+  [kw]
+  (-> (name kw)
+      (str/lower-case)
+      val->string))
 
 (defn- map->kw-args
   "convert a map into a stringified list of Python keyword arguments"
   [m]
-  (cs/join "," (map (fn [[k v]]
-                      (str (keyword->argname k) "=" (val->string v))) m)))
+  (format "{%s}"
+          (str/join "," (map (fn [[k v]]
+                               (str (keyword->argname k) ":" (val->string v)))
+                             m))))
 
 (defn highlight
   "highlight a piece of code."
   ([code lang output] (highlight code lang output {}))
   ([code lang output opts]
-    (let [opt-args (map->kw-args opts)]
-      ;; we give all options to both lexer and formatter, unknown ones will
-      ;; be ignored
-      (exec-code-in "_r" (format "get_lexer_by_name(\"%s\",%s)"
-                                 (keyword->langname lang) opt-args) "None")
-      (exec-code-in "_f" (format "get_formatter_by_name(\"%s\",%s)"
-                                 (keyword->langname output) opt-args) "None")
-      (if (py-true? "_r!=None" "_f!=None")
-        (do
-          (.exec python (str "_res=highlight(\"\"\"" (escape-string code)
-                             "\"\"\",_r,_f)\n"))
-          ;; This won't work with non-text formatters, such as gif, but it's
-          ;; ok since they're not supported on Jython (#2)
-          (.get python "_res" String))))))
+   {:pre [(string? code)]}
+   (let [opt-args (map->kw-args opts)
+         ;; We give all options to both lexer and formatter;
+         ;; unknown ones are ignored.
+         python-code (format "run(%s, %s, %s, %s, %s)"
+                             (val->string code)
+                             (keyword->langname lang) opt-args
+                             (keyword->langname output) opt-args)
+
+         res ^PyObject (.eval ^PythonInterpreter python python-code)]
+
+     (when (and res (.__nonzero__ res))
+       ;; This won't work with non-text formatters, such as gif, but it's
+       ;; ok since they're not supported on Jython (#2)
+       (.asString res)))))
--- test/clygments/core_test.clj
+++ test/clygments/core_test.clj
@@ -2,69 +2,29 @@
   (:require [clojure.test :refer :all]
             [clygments.core :as clg]))
 
-(deftest python-interpreter
-  (is (= 16 (.getValue (.eval @#'clg/python "pow(4,2)")))))
-
-(deftest exec-code-in
-  (let [v "__foo42"]
-    (testing "without default"
-      (#'clg/exec-code-in v "41 + 1")
-      (is (= 42 (.getValue (.get @#'clg/python v)))))
-    (testing "with default value but no exception"
-      (#'clg/exec-code-in v "41 + 1" "45")
-      (is (= 42 (.getValue (.get @#'clg/python v)))))
-    (testing "with default value"
-      (#'clg/exec-code-in v "1/0" "45")
-      (is (= 45 (.getValue (.get @#'clg/python v)))))))
-
-(deftest py-true?
-  (testing "True is True"
-    (is (= (#'clg/py-true? "True") true)))
-  (testing "Truthy is not True"
-    (is (= (#'clg/py-true? "42") false)))
-  (testing "a true expression is True"
-    (is (= (#'clg/py-true? "1+1 == 2") true))
-    (is (= (#'clg/py-true? "1+1 != 2") false)))
-  (testing "multiple expressions"
-    (is (= (#'clg/py-true? "1+1 == 2" "2+2 == 4") true))
-    (is (= (#'clg/py-true? "1+1 == 2" "2+1 == 4") false))))
-
-(deftest escape-string
-  (testing "empty string"
-    (is (= (#'clg/escape-string "") "")))
-  (testing "no special chars"
-    (let [s "foobar 42 #$"]
-      (is (= (#'clg/escape-string s) s))))
-  (testing "backslashes only"
-    (is (= (#'clg/escape-string "here: \\") "here: \\\\")))
-  (testing "special \\-chars"
-    (is (= (#'clg/escape-string "newline: \\n") "newline: \\\\n")))
-  (testing "quotes"
-    (is (= (#'clg/escape-string "a \"string\".") "a \\\"string\\\"."))))
-
 (deftest keyword->argname
   (testing "one word"
-    (is (= (#'clg/keyword->argname :foo) "foo")))
+    (is (= (#'clg/keyword->argname :foo) "\"foo\"")))
   (testing "capitalized"
-    (is (= (#'clg/keyword->argname :Foo) "foo")))
+    (is (= (#'clg/keyword->argname :Foo) "\"foo\"")))
   (testing "with an hyphen"
-    (is (= (#'clg/keyword->argname :foo-bar) "foobar")))
+    (is (= (#'clg/keyword->argname :foo-bar) "\"foobar\"")))
   (testing "with an hyphen and capitalized"
-    (is (= (#'clg/keyword->argname :Foo-Bar) "foobar")))
+    (is (= (#'clg/keyword->argname :Foo-Bar) "\"foobar\"")))
   (testing "string"
-    (is (= (#'clg/keyword->argname "Foo-Bar") "foobar"))))
+    (is (= (#'clg/keyword->argname "Foo-Bar") "\"foobar\""))))
 
 (deftest keyword->langname
   (testing "one word"
-    (is (= (#'clg/keyword->langname :foo) "foo")))
+    (is (= (#'clg/keyword->langname :foo) "\"foo\"")))
   (testing "capitalized"
-    (is (= (#'clg/keyword->langname :Foo) "foo")))
+    (is (= (#'clg/keyword->langname :Foo) "\"foo\"")))
   (testing "with an hyphen"
-    (is (= (#'clg/keyword->langname :foo-bar) "foo-bar")))
+    (is (= (#'clg/keyword->langname :foo-bar) "\"foo-bar\"")))
   (testing "with an hyphen and capitalized"
-    (is (= (#'clg/keyword->langname :Foo-Bar) "foo-bar")))
+    (is (= (#'clg/keyword->langname :Foo-Bar) "\"foo-bar\"")))
   (testing "string"
-    (is (= (#'clg/keyword->langname "Foo-Bar") "foo-bar"))))
+    (is (= (#'clg/keyword->langname "Foo-Bar") "\"foo-bar\""))))
 
 (deftest val->string
   (testing "nil"
@@ -94,16 +54,16 @@
 
 (deftest map->kw-args
   (testing "empty map"
-    (is (= (#'clg/map->kw-args {}) "")))
+    (is (= (#'clg/map->kw-args {}) "{}")))
   (testing "string->string"
-    (is (= (#'clg/map->kw-args {"foo" "bar"}) "foo=\"bar\""))
-    (is (= (#'clg/map->kw-args {"foo" "b\\ar"}) "foo=\"b\\\\ar\""))
-    (is (= (#'clg/map->kw-args {"foo" "bar\\n"}) "foo=\"bar\\\\n\"")))
+    (is (= (#'clg/map->kw-args {"foo" "bar"}) "{\"foo\":\"bar\"}"))
+    (is (= (#'clg/map->kw-args {"foo" "b\\ar"}) "{\"foo\":\"b\\\\ar\"}"))
+    (is (= (#'clg/map->kw-args {"foo" "bar\\n"}) "{\"foo\":\"bar\\\\n\"}")))
   (testing "keyword->string"
-    (is (= (#'clg/map->kw-args {:foo "bar"}) "foo=\"bar\"")))
+    (is (= (#'clg/map->kw-args {:foo "bar"}) "{\"foo\":\"bar\"}")))
   (testing "various types"
     (is (= (#'clg/map->kw-args (sorted-map :a "a" :b 42.0 :c nil))
-           "a=\"a\",b=42.0,c=None"))))
+           "{\"a\":\"a\",\"b\":42.0,\"c\":None}"))))
 
 (deftest highlight
   (testing "unknown language"
