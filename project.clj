3e6c433cd1dea9aa4ca524ac9d04c6839e3a58a0
Baptiste Fontaine <b@ptistefontaine.fr>
2019-03-18 21:41:56 +0100

    bump deps

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure          "1.8.0"]
-                 [org.python/jython-standalone "2.7.0"]
-                 [org.pygments/pygments        "2.1.3"]]
+                 [org.python/jython-standalone "2.7.1"]
+                 [org.pygments/pygments        "2.3.1"]]
   :plugins [[lein-cloverage "1.0.9"]])
