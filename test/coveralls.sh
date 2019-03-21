d7821ac7bd1173b5c90d5d6f5687d14fd9a0483a
Baptiste Fontaine <b@ptistefontaine.fr>
2019-03-21 22:47:37 +0100

    Fix Travis job

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,2 +1,2 @@
-CLOVERAGE_VERSION='1.0.4-SNAPSHOT' lein2 cloverage -o cov --coveralls
+CLOVERAGE_VERSION='1.0.4-SNAPSHOT' lein cloverage -o cov --coveralls
 curl -F 'json_file=@cov/coveralls.json' 'https://coveralls.io/api/v1/jobs'
