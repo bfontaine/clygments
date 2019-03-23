bf304945b76aa359efc96a0182d507b4f7e7ab5c
Baptiste Fontaine <b@ptistefontaine.fr>
2019-03-23 18:04:19 +0100

    Replace Python code with Clython (WIP)

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject clygments "2.0.0"
+(defproject clygments "2.0.0-clython-SNAPSHOT"
   :description "Use Pygments from Clojure"
   :url "https://github.com/bfontaine/clygments"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure          "1.8.0"]
                  [org.python/jython-standalone "2.7.1"]
-                 [org.pygments/pygments        "2.3.1"]]
+                 [org.pygments/pygments        "2.3.1"]
+                 [clython                      "0.0.1"]]
   :plugins [[lein-cloverage "1.0.9"]])
--- src/clygments/core.clj
+++ src/clygments/core.clj
@@ -1,109 +1,47 @@
 (ns clygments.core
-  (:require [clojure.string :as str])
-  (:import [org.python.util PythonInterpreter]
-           [org.python.core PyObject PyNone PyBoolean PyLong PyFloat
-                            PyDictionary PyUnicode]
-           [java.util HashMap]
-           [clojure.lang Keyword PersistentArrayMap PersistentHashMap]))
+  (:require [clojure.string :as str]
+            [clython.core :as cly])
+  (:import [org.python.core PyException]))
 
-(def ^:private python
-  (doto (PythonInterpreter.)
-    (.exec "
-from pygments import highlight
-from pygments.lexers import get_lexer_by_name, guess_lexer
-from pygments.formatters import get_formatter_by_name
-from pygments.util import ClassNotFound
+(defn- ignore-py-ex
+  [f arg]
+  (try
+    (f arg)
+    (catch PyException _ nil)))
 
-def get_lexer(name, opts):
-    try:
-        return get_lexer_by_name(name, **opts)
-    except:
-        pass
+(cly/let-import [pygments [highlight]
+                 pygments.lexers [get-lexer-by-name guess-lexer]
+                 pygments.formatters [get-formatter-by-name]]
+  (defn- highlight*
+    ; opts unsupported for now because get-*-by-name want kwargs-style args and
+    ; we don't support those in our IFn reify thing
+    [code lexer-name formatter-name opts]
+    (let [lexer (if lexer-name
+                  (ignore-py-ex get-lexer-by-name lexer-name)
+                  (guess-lexer code))
 
-def get_formatter(name, opts):
-    try:
-        return get_formatter_by_name(name, **opts)
-    except:
-        pass
+          formatter (ignore-py-ex get-formatter-by-name formatter-name)]
 
-def run(code, lexer_name, formatter_name, opts):
-    if lexer_name is None:
-        try:
-           lexer = guess_lexer(code)
-        except ClassNotFound:
-           return
-    else:
-        lexer = get_lexer(lexer_name, opts)
+      (when (and lexer formatter)
+        (highlight code lexer formatter)))))
 
-    formatter = get_formatter(formatter_name, opts)
-
-    if lexer and formatter:
-        return highlight(code, lexer, formatter)")))
-
-(def ^:private
-  py-run-fn
-  (.eval python "run"))
-
-(def ^:private
-  py-none (.eval ^PythonInterpreter python "None"))
-
-(defmulti ^:private clj->jy type)
-
-(defmethod clj->jy nil [_] py-none)
-
-(let [py-true (PyBoolean. true)
-      py-false (PyBoolean. false)]
-  (defmethod clj->jy Boolean [x]
-    (if x
-      py-true
-      py-false)))
-
-(defmethod clj->jy String [s] (PyUnicode. ^String s))
-(defmethod clj->jy Integer [i] (PyLong. ^Long (long i)))
-(defmethod clj->jy Long [l] (PyLong. ^Long l))
-(defmethod clj->jy Float [f] (PyFloat. ^Float f))
-(defmethod clj->jy Double [d] (PyFloat. ^Double d))
-(defmethod clj->jy Character [c] (PyUnicode. (str c)))
-(defmethod clj->jy Keyword [k] (PyUnicode. (name k)))
-
-(defn- map->jy
-  ([m]
-   (map->jy m identity))
-  ([m keyfn]
-   (let [hm (HashMap. (count m))]
-     (doseq [[k v] m]
-       (.put hm (clj->jy (keyfn k)) (clj->jy v)))
-     (PyDictionary. hm))))
-
-(defmethod clj->jy PersistentHashMap [m] (map->jy m))
-(defmethod clj->jy PersistentArrayMap [m] (map->jy m))
-
-(defn- keyword->lowercase-pystring
+(defn- keyword->lowercase
   [kw]
-  (-> kw name str/lower-case (PyUnicode.)))
+  (-> kw name str/lower-case))
 
 (defn highlight
   "highlight a piece of code."
   ([code lang output] (highlight code lang output nil))
   ([code lang output opts]
    {:pre [(string? code)]}
-   (let [py-code (PyUnicode. ^String code)
-         py-lexer-name (if (some? lang)
-                         (keyword->lowercase-pystring lang)
-                         py-none)
-         py-formatter-name (keyword->lowercase-pystring output)
+   (let [lexer-name (if (some? lang)
+                      (keyword->lowercase lang))
+         formatter-name (keyword->lowercase output)
 
-         py-opts (map->jy opts
-                          (fn [k]
-                            (-> k name (str/replace #"-" "") str/lower-case)))
+         opts (->> opts
+                   (map (fn [[k v]]
+                          [(-> k name (str/replace #"-" "") str/lower-case)
+                           v]))
+                   (into {}))]
 
-         res (.__call__ py-run-fn
-                        (into-array PyObject [py-code
-                                              py-lexer-name
-                                              py-formatter-name
-                                              py-opts]))]
-
-     (when (and res (.__nonzero__ res))
-       ;; This won't work with non-text formatters such as :gif, but it's
-       ;; ok since they're not supported on Jython (#2)
-       (.asString res)))))
+     (cly/jy->clj (highlight* code lexer-name formatter-name opts)))))
