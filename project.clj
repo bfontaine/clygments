2501cecf56de75329d2a997e32f3feab94d2543d
Baptiste Fontaine <b@ptistefontaine.fr>
2020-09-06 21:30:53 +0200

    Bump pygments to 2.5.2

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure          "1.8.0"]
                  [org.python/jython-standalone "2.7.1"]
-                 [org.pygments/pygments        "2.3.1"]]
+                 [org.pygments/pygments        "2.5.2"]]
   :plugins [[lein-cloverage "1.0.9"]])
