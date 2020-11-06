99c806e774f91ce318b635d9f520042e74ddeef0
Mathieu Lirzin <mthl@reuz.fr>
2020-11-06 23:17:08 +0100

    Bump clojure version
    Make it a dev dependencies to ensure that it does not impact the
    version used by dependant projects.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/bfontaine/clygments"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure          "1.8.0"]
-                 [org.python/jython-standalone "2.7.1"]
+  :dependencies [[org.python/jython-standalone "2.7.1"]
                  ;; Pygments 2.6+ doesn't support Python2 anymore, so we'll have to ditch Jython in favor of GraalVM
                  [org.pygments/pygments        "2.5.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
   :plugins [[lein-cloverage "1.0.9"]])
