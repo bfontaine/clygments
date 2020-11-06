12cee30b3db598c689788ea5ffcaddf0eacf3c00
Mathieu Lirzin <mthl@reuz.fr>
2020-11-06 22:01:19 +0100

    Warn on reflection

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
                  ;; Pygments 2.6+ doesn't support Python2 anymore, so we'll have to ditch Jython in favor of GraalVM
                  [org.pygments/pygments        "2.5.2"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}}
+  :global-vars {*warn-on-reflection* true}
   :plugins [[lein-cloverage "1.0.9"]])
